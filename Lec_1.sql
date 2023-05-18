63573601a3d4d89f4cfd2cf7d70ccbfb1bbf131f
Julia Ch <juliecha@yandex.ru>
2023-05-18 09:22:00 +0300

    UPD Lec_1

--- Lec_1.sql
+++ Lec_1.sql
@@ -1,6 +1,12 @@
 USE gb_myfirstdb;
-SELECT *  FROM test;
+SELECT *  FROM students;
 
-SELECT name, pass
-FROM test
+SELECT fio, pass
+FROM students
 WHERE login = 'test123';
+
+INSERT INTO students (fio, login, pass, email) VALUES ('Dmitrev Dmitrii Dmitrievich', 'test12345', '12345', 'test12345@gmail.com');
+INSERT INTO students (fio, login, pass, email) VALUES ('Fdfsdgf kkkk Dbgfmgm', 'test123456', '123456', 'test123456@gmail.com');
+SELECT *  FROM students;
+
+DELETE FROM students WHERE idtest = '7';
