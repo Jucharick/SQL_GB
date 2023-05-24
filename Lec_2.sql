8230f790fc2339e278cd810d8d8410b9222c8fb4
Julia Ch <juliecha@yandex.ru>
2023-05-24 19:48:41 +0300

    UPD Lec_2

--- Lec_2.sql
+++ Lec_2.sql
@@ -1,6 +1,14 @@
 show databases;
 use gb_db;
 
+# здесь комментарий
+
+-- здесь комментарий
+
+/* и здесь тоже комментарий 
+на несколько
+строк*/
+
 /*CREATE TABLE table_name
 (
 column_name_1 column_type_1,
@@ -28,3 +36,37 @@ CustomerId INT,
 CreatedAt Date,
 FOREIGN KEY (CustomerId) REFERENCES Customers (Id) -- внешний ключ из таблицы Customer
 );
+
+-- Арифметические операции
+SELECT 3+5;
+SELECT 3-5;
+SELECT 3*5;
+SELECT 3/5;
+SELECT 102/0;
+
+
+-- Логические операторы
+
+CREATE TABLE Products
+(
+Id INT AUTO_INCREMENT PRIMARY KEY,
+ProductName VARCHAR(30) NOT NULL,
+Manufacturer VARCHAR(20) NOT NULL,
+ProductCount INT DEFAULT 0,
+Price DECIMAL
+);
+
+-- AND: операция логического И
+SELECT * FROM Products
+WHERE Manufacturer = 'Samsung' AND Price > 50000;
+
+-- OR: операция логического ИЛИ
+SELECT * FROM Products
+WHERE Manufacturer = 'Samsung' OR Price > 50000;
+
+-- NOT: операция логического отрицания
+SELECT * FROM Products
+WHERE NOT Manufacturer = 'Samsung';
+
+
+
