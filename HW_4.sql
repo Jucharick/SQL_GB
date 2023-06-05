40b1a2a5bf0335071ad1c25e507401ec30602043
Julia Ch <juliecha@yandex.ru>
2023-06-05 20:44:31 +0300

    UPD HW_4

--- HW_4.sql
+++ HW_4.sql
@@ -3,81 +3,7 @@ USE home_work_4;
 
 
 -- Создание таблиц
-create table users
-(
-    id  int auto_increment primary key,
-    login varchar(255) null,
-    pass  varchar(255) null,
-    male  tinyint      null
-);
 
-create table clients
-(
-    id    int auto_increment primary key,
-    login varchar(255) null,
-    pass  varchar(255) null,
-    male  tinyint      null
-);
-
-INSERT INTO users (login, pass, male) VALUES ('alex', '$2y$10$6SzbBCMENklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH14y', 1);
-INSERT INTO users (login, pass, male) VALUES ('Mikle', '$ws$10$6SzbBCMENklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH14y', 1);
-INSERT INTO users (login, pass, male) VALUES ('Olia', '$2y$10$88zbBCKLJklStIgTqBKIluijJUnbeZ5WqJI4RJgkksnFZon5kH14y', 2);
-INSERT INTO users (login, pass, male) VALUES ('Tom', '$2y$20$6SzbBCNRNklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH20y', 1);
-INSERT INTO users (login, pass, male) VALUES ('Margaret', '$2y$20$6SzbBCNRNklStIgTqBKIluijJUnbeZ4wqIu4RJgkksnFZon5kH20y', 2);
-INSERT INTO users (login, pass, male) VALUES ('alex', '$2y$10$6SzbBCMENklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH14y', 1);
-
-INSERT INTO clients (login, pass, male) VALUES ('alexander', '$2y$10$6SzbBCMENklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH14y', 1);
-INSERT INTO clients (login, pass, male) VALUES ('Mikle', '$ws$10$6SzbBCMENklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH14y', 1);
-INSERT INTO clients (login, pass, male) VALUES ('Olia', '$2y$10$88zbBCKLJklStIgTqBKIluijJUnbeZ5WqJI4RJgkksnFZon5kH14y', 2);
-INSERT INTO clients (login, pass, male) VALUES ('Dmitry', '$2y$20$6SzbBCNRNklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH20y', 1);
-INSERT INTO clients (login, pass, male) VALUES ('Margaret', '$2y$20$6SzbBCNRNklStIgTqBKIluijJUnbeZ4wqIu4RJgkksnFZon5kH20y', 2);
-INSERT INTO clients (login, pass, male) VALUES ('Leonid', '$2y$10$6SzbBCMENklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH14y', 1);
-INSERT INTO clients (login, pass, male) VALUES ('Mikle', '$ws$10$6SzbBCMENklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH14y', 1);
-INSERT INTO clients (login, pass, male) VALUES ('Olga', '$2y$10$88zbBCKLJklStIgTqBKIluijJUnbeZ5WqJI4RJgkksnFZon5kH14y', 2);
-INSERT INTO clients (login, pass, male) VALUES ('Tom', '$2y$20$6SzbBCNRNklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH20y', 1);
-INSERT INTO clients (login, pass, male) VALUES ('Masha', '$2y$20$6SzbBCNRNklStIgTqBKIluijJUnbeZ4wqIu4RJgkksnFZon5kH20y', 2);
-INSERT INTO clients (login, pass, male) VALUES ('alex', '$2y$10$6SzbBCMENklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH14y', 1);
-
-
--- Задание на EXISTS
-CREATE TABLE Employee (
-  Id INT PRIMARY KEY,
-  Name VARCHAR(45) NOT NULL,
-  Department VARCHAR(45) NOT NULL,
-  Salary FLOAT NOT NULL,
-  Gender VARCHAR(45) NOT NULL,
-  Age INT NOT NULL,
-  City VARCHAR(45) NOT NULL
-);
-INSERT INTO Employee (Id, `Name`, Department, Salary, Gender, Age, City) VALUES (1001, 'John Doe', 'IT', 35000, 'Male', 25, 'London');
-INSERT INTO Employee (Id, `Name`, Department, Salary, Gender, Age, City) VALUES (1002, 'Mary Smith', 'HR', 45000, 'Female', 27, 'London');
-INSERT INTO Employee (Id, `Name`, Department, Salary, Gender, Age, City) VALUES (1003, 'James Brown', 'Finance', 50000, 'Male', 28, 'London');
-INSERT INTO Employee (Id, `Name`, Department, Salary, Gender, Age, City) VALUES (1004, 'Mike Walker', 'Finance', 50000, 'Male', 28, 'London');
-INSERT INTO Employee (Id, `Name`, Department, Salary, Gender, Age, City) VALUES (1005, 'Linda Jones', 'HR', 75000, 'Female', 26, 'London');
-INSERT INTO Employee (Id, `Name`, Department, Salary, Gender, Age, City) VALUES (1006, 'Anurag Mohanty', 'IT', 35000, 'Male', 25, 'Mumbai');
-INSERT INTO Employee (Id, `Name`, Department, Salary, Gender, Age, City) VALUES (1007, 'Priyanla Dewangan', 'HR', 45000, 'Female', 27, 'Mumbai');
-INSERT INTO Employee (Id, `Name`, Department, Salary, Gender, Age, City) VALUES (1008, 'Sambit Mohanty', 'IT', 50000, 'Male', 28, 'Mumbai');
-INSERT INTO Employee (Id, `Name`, Department, Salary, Gender, Age, City) VALUES (1009, 'Pranaya Kumar', 'IT', 50000, 'Male', 28, 'Mumbai');
-INSERT INTO Employee (Id, `Name`, Department, Salary, Gender, Age, City) VALUES (1010, 'Hina Sharma', 'HR', 75000, 'Female', 26, 'Mumbai');
-CREATE TABLE Projects (
- ProjectId INT PRIMARY KEY AUTO_INCREMENT,
-      Title VARCHAR(200) NOT NULL,
-     ClientId INT,
- EmployeeId INT,
-     StartDate DATETIME,
-     EndDate DATETIME
-);
-INSERT INTO Projects ( Title, ClientId, EmployeeId, StartDate, EndDate) VALUES 
-('Develop ecommerse website from scratch', 1, 1003, NOW(), DATE_ADD(NOW(), INTERVAL 30 DAY)),
-('WordPress website for our company', 1, 1002, NOW(), DATE_ADD(NOW(), INTERVAL 45 DAY)),
-('Manage our company servers', 2, 1007, NOW(), DATE_ADD(NOW(), INTERVAL 45 DAY)),
-('Hosting account is not working', 3, 1009, NOW(), DATE_ADD(NOW(), INTERVAL 7 DAY)),
-('MySQL database from my desktop application', 4, 1010, NOW(), DATE_ADD(NOW(), INTERVAL 15 DAY)),
-('Develop new WordPress plugin for my business website', 2, NULL, NOW(), DATE_ADD(NOW(), INTERVAL 10 DAY)),
-('Migrate web application and database to new server', 2, NULL, NOW(), DATE_ADD(NOW(), INTERVAL 5 DAY)),
-('Android Application development', 4, 1004, NOW(), DATE_ADD(NOW(), INTERVAL 30 DAY));
-
--- Собеседования
 CREATE TABLE  AUTO 
 (       
 	REGNUM VARCHAR(10) PRIMARY KEY, 
@@ -88,129 +14,25 @@ CREATE TABLE  AUTO
 );
 
 
-CREATE TABLE  CITY 
-(	
-    CITYCODE INT PRIMARY KEY,
-	CITYNAME VARCHAR(50), 
-	PEOPLES INT 
-);
-
-
-CREATE TABLE  MAN 
-(	
-	PHONENUM VARCHAR(15) PRIMARY KEY , 
-	FIRSTNAME VARCHAR(50),
-	LASTNAME VARCHAR(50),  
-	CITYCODE INT, 
-	YEAROLD INT	 
-);
-
-
  -- AUTO
 INSERT INTO AUTO (REGNUM, MARK,	COLOR, RELEASEDT, PHONENUM )
 VALUES(111114,'LADA', 'КРАСНЫЙ', date'2008-01-01', '9152222221');
-
-
 INSERT INTO AUTO (REGNUM, MARK,	COLOR, RELEASEDT, PHONENUM )
 VALUES(111115,'VOLVO', 'КРАСНЫЙ', date'2013-01-01', '9173333334');
-
-
 INSERT INTO AUTO (REGNUM, MARK,	COLOR, RELEASEDT, PHONENUM )
 VALUES(111116,'BMW', 'СИНИЙ', date'2015-01-01', '9173333334');
-
-
 INSERT INTO AUTO (REGNUM, MARK,	COLOR, RELEASEDT, PHONENUM )
 VALUES(111121,'AUDI', 'СИНИЙ', date'2009-01-01', '9173333332');
-
-
 INSERT INTO AUTO (REGNUM, MARK,	COLOR, RELEASEDT, PHONENUM )
 VALUES(111122,'AUDI', 'СИНИЙ', date'2011-01-01', '9213333336');
-
-
 INSERT INTO AUTO (REGNUM, MARK,	COLOR, RELEASEDT, PHONENUM )
 VALUES(111113,'BMW', 'ЗЕЛЕНЫЙ', date'2007-01-01', '9214444444');
-
-
 INSERT INTO AUTO (REGNUM, MARK,	COLOR, RELEASEDT, PHONENUM )
 VALUES(111126,'LADA', 'ЗЕЛЕНЫЙ', date'2005-01-01', null);
-
-
 INSERT INTO AUTO (REGNUM, MARK,	COLOR, RELEASEDT, PHONENUM )
 VALUES(111117,'BMW', 'СИНИЙ', date'2005-01-01', null);
-
-
 INSERT INTO AUTO (REGNUM, MARK,	COLOR, RELEASEDT, PHONENUM )
 VALUES(111119,'LADA', 'СИНИЙ', date'2017-01-01', 9213333331);
-
-
- -- CITY
-INSERT INTO CITY (CITYCODE,CITYNAME,PEOPLES)
-VALUES(1,'Москва', 10000000);
-
-
-INSERT INTO CITY (CITYCODE,CITYNAME,PEOPLES)
-VALUES(2,'Владимир', 500000);
-
-
-INSERT INTO CITY (CITYCODE,CITYNAME,PEOPLES)
-VALUES(3, 'Орел', 300000);
-
-
-INSERT INTO CITY (CITYCODE,CITYNAME,PEOPLES)
-VALUES(4,'Курск', 200000);
-
-
-INSERT INTO CITY (CITYCODE,CITYNAME,PEOPLES)
-VALUES(5, 'Казань', 2000000);
-
-
-INSERT INTO CITY (CITYCODE,CITYNAME,PEOPLES)
-VALUES(7, 'Котлас', 110000);
-
-
-INSERT INTO CITY (CITYCODE,CITYNAME,PEOPLES)
-VALUES(8, 'Мурманск', 400000);
-
-
-INSERT INTO CITY (CITYCODE,CITYNAME,PEOPLES)
-VALUES(9, 'Ярославль', 500000);
-
--- MAN
-INSERT INTO MAN (PHONENUM,FIRSTNAME,LASTNAME,CITYCODE,YEAROLD)
-VALUES('9152222221','Андрей','Николаев', 1, 22);
-
-
-INSERT INTO MAN (PHONENUM,FIRSTNAME,LASTNAME,CITYCODE,YEAROLD)
-VALUES('9152222222','Максим','Москитов', 1, 31);
-
-
-INSERT INTO MAN (PHONENUM,FIRSTNAME,LASTNAME,CITYCODE,YEAROLD)
-VALUES('9153333333','Олег','Денисов', 3, 34);
-
-
-INSERT INTO MAN (PHONENUM,FIRSTNAME,LASTNAME,CITYCODE,YEAROLD)
-VALUES('9173333334','Алиса','Никина', 4, 31);
-
-
-INSERT INTO MAN (PHONENUM,FIRSTNAME,LASTNAME,CITYCODE,YEAROLD)
-VALUES('9173333335','Таня','Иванова', 4, 31);
-
-
-INSERT INTO MAN (PHONENUM,FIRSTNAME,LASTNAME,CITYCODE,YEAROLD)
-VALUES('9213333336','Алексей','Иванов', 7, 25);
-
-
-INSERT INTO MAN (PHONENUM,FIRSTNAME,LASTNAME,CITYCODE,YEAROLD)
-VALUES('9213333331','Андрей','Некрасов', 2, 27);
-
-
-INSERT INTO MAN (PHONENUM,FIRSTNAME,LASTNAME,CITYCODE,YEAROLD)
-VALUES('9213333332','Миша','Рогозин', 2, 21);
-
-
-INSERT INTO MAN (PHONENUM,FIRSTNAME,LASTNAME,CITYCODE,YEAROLD)
-VALUES('9214444444','Алексей','Галкин', 1, 38);
-
 
 
 -- Задания
@@ -226,9 +48,11 @@ ORDER BY MARK ;
 -- марки.
 -- 100 машин, их них 20 - BMW и 80 машин другой марки , AUDI - 30 и 70 машин другой
 -- марки, LADA - 15, 85 авто другой марки
-SELECT MARK, count(1) AS 'Этой_марки'
-FROM AUTO
-GROUP BY MARK;
+SELECT MARK, 
+  	   COUNT(1) AS 'Этой_марки', 
+       (SELECT COUNT(1) FROM AUTO WHERE MARK <> A.MARK) AS 'Всего_других' 
+FROM AUTO A
+GROUP BY MARK; 
 
 -- 3. Даны 2 таблицы, созданные следующим образом. Напишите запрос, который вернет строки из таблицы test_a, id которых нет в
 -- таблице test_b, НЕ используя ключевого слова NOT.
@@ -251,4 +75,48 @@ FROM test_a AS a
 LEFT JOIN test_b AS b 
 ON a.id = b.id
 WHERE b.id IS NULL;
+
+
+-- Дополнительное задание
+-- Необходимо преобразовать поля таблицы к типу DATETIME, сохранив введеные ранее значения.
+CREATE TABLE IF NOT EXISTS orders (
+onum INT NOT NULL,
+amt DECIMAL(7,2) NOT NULL,
+odate VARCHAR(10) NOT NULL,
+cnum INT,
+snum INT,
+PRIMARY KEY (onum)
+);
+
+INSERT INTO orders(onum, amt, odate, cnum, snum)
+VALUES
+(3001, 18.69, '10/03/1990', 2008, 1007),
+(3003, 767.19, '10/03/1990', 2001, 1001),
+(3002, 1900.10, '10/03/1990', 2007, 1004),
+(3005, 5160.45, '10/03/1990', 2003, 1002),
+(3006, 1098.16, '10/03/1990', 2008, 1007),
+(3009, 1713.23, '10/04/1990', 2002, 1003),
+(3007, 75.75, '10/04/1990', 2004, 1002),
+(3008, 4723.00, '10/05/1990', 2006, 1001),
+(3010, 1309.95, '10/06/1990', 2004, 1002),
+(3011, 9891.88, '10/06/1990', 2006, 1001);
+
+SELECT * FROM orders;;
+
+-- копирую таблицу и форматирую строку odate
+CREATE TABLE orders_new
+SELECT onum, amt, 
+	   CONCAT(RIGHT(o.odate, 4), "/", SUBSTRING(o.odate, 4,2), "/", LEFT(o.odate, 2)) AS odate,
+	   cnum, snum
+FROM orders o;;
+
+-- меняю тип данных в атрибуте odate
+ALTER TABLE orders_new
+MODIFY COLUMN odate DATETIME;
+
+-- проверяю тип данных
+DESC orders_new;;
+
+SELECT onum, amt, monthname(odate)
+FROM orders_new;;
 
